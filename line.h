a45a93fdedc4d220f0cac7cf706b5b452be8a4cd
Mblacha10 <marcel.blicharski@woli.edu.pl>
2024-12-09 09:36:33 +0000

    chocho_milky

--- line.h
+++ line.h
@@ -1,4 +1,4 @@
-#ifndef LINE_H
+/*#ifndef LINE_H
 #define LINE_H
 class Line{
     private:
@@ -9,4 +9,35 @@ class Line{
     Line(Point &other);
 
 };
-#endif
+#endif*/
+//
+// Created by nwaszkowiak on 26.11.24.
+//
+
+#ifndef PO_LAB3_FIGURES_LINE_H
+#define PO_LAB3_FIGURES_LINE_H
+
+#include "point.h"
+
+
+class Line {
+private:
+    Point a;
+    Point b;
+public:
+    Line(Point a, Point b);
+
+    Line(const Line &other);
+
+    bool equals(Line &other);
+
+    void flip();
+
+    void move(double x, double y);
+
+    std::string toString();
+
+};
+
+
+#endif //PO_LAB3_FIGURES_LINE_H
